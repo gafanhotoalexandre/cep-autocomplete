df62fcbc014f49ea6bd528b29ec90bee02e40767
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-06 15:43:28 -0300

    selecting elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log('current');
+const addressForm = document.querySelector('#address-form');
+const cepInput = document.querySelector('#cep');
+const addressInput = document.querySelector('#address');
+const cityInput = document.querySelector('#city');
+const neighborhoodInput = document.querySelector('#neighborhood');
+const regionInput = document.querySelector('#region');
+const formInputs = document.querySelectorAll('[data-input]');
+
+const closeButton = document.querySelector('#close-message');

b9848c9ccc5bcec72b0820f7f92d4b0949812486
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-06 16:38:51 -0300

    request for viacep api

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,39 @@ cepInput.addEventListener('keypress', (event) => {
   }
 });
 
+// get address event
+cepInput.addEventListener('keyup', event => {
+  const inputValue = event.target.value;
+
+  // check if we have the correct length
+  if (inputValue.length === 8) getAddress(inputValue);
+});
+
+// get costumer address from api
+async function getAddress(cep) {
+  toggleLoader();
+  cepInput.blur();
+
+  // request
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  // show error and reset form
+  if (data.erro === 'true') {
+    addressForm.reset();
+    toggleLoader();
+    // show message
+    return;
+  }
+}
+
+// show or hide loader
+function toggleLoader() {
+  const fadeElement = document.querySelector('#fade');
+  const loaderElement = document.querySelector('#loader');
+
+  fadeElement.classList.toggle('hide');
+  loaderElement.classList.toggle('hide');
+}
